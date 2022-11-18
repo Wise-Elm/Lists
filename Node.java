8717b20efd9aaeb264fdb4ab8031484fceb0754d
Graham Steeds <70621459+Wise-Elm@users.noreply.github.com>
2022-11-17 20:17:32 -0600

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,35 @@
+public class Node {
+
+    private int value;
+    private Node next;
+
+    public Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    public void setNodeValue(int value) {
+        // Sets node value.
+        this.value = value;
+    }
+
+    public int getNodeValue() {
+        // Returns node value.
+        return this.value;
+    }
+
+    public void setNodeNext(Node next) {
+        // Sets next node.
+        this.next = next;
+    }
+
+    public Node getNodeNext() {
+        // Returns next node.
+        return this.next;
+    }
+
+    public void removeNodeNext() {
+        // Remove node next
+        this.next = null;
+    }
+}
